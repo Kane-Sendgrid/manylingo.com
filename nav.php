946bdf7dedb304210e1636e2f93d55a37d5266f1
Kane <kane@sendgrid.com>
2016-09-18 23:50:51 -0700

    content fixes

--- nav.php
+++ nav.php
@@ -4,7 +4,12 @@ require_once("./functions.php");
 <ul class="nav navbar-nav main-navigation">
     <li class="<?php echo activeClass("/"); ?>"><a href="/">Home</a></li>
     <li class="<?php echo activeClass("/about.html"); ?>"><a href="/about.html">About</a></li>
-    <li class="<?php echo activeClass("/services.html"); ?>"><a href="/services.html">Services</a></li>
+    <li class="<?php echo activeClass("/services.html"); ?> dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+            <li><a href="/services.html">Services</a></li>
+        </ul>
+    </li>
     <li class="<?php echo activeClass("/prices.html"); ?>"><a href="/prices.html">Prices</a></li>
     <li class="<?php echo activeClass("/faq.html"); ?>"><a href="/faq.html">FAQ</a></li>
     <li><a href="#contact">Contact</a></li>
