1d142c59bd324507627618bce3985e9c8c7936fe
Kane <kane@sendgrid.com>
2016-09-19 01:21:08 -0700

    more fixes

---
+++ footer.php
@@ -0,0 +1,76 @@
+<section id="footer">
+    <div class="container">
+    <div class="container">
+        <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <h3>Services</h3>
+            <ul>
+            <li><a href="/services.html">Translation</a></li>
+            </ul>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <h3>FAQs</h3>
+            <ul>
+            <li><a href="/faq.html#21">Languages</a></li>
+            <li><a href="/faq.html#7">Fees</a></li>
+            <li><a href="/faq.html#5">Guarantee</a></li>
+            <li><a href="/faq.html#31">Support</a></li>
+            </ul>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <h3>About</h3>
+            <ul>
+            <li><a href="/about.html">Who we are</a>
+            </li>
+            </ul>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <h3>Find us on</h3>
+            <a class="social" href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+            <a class="social" href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+        </div>
+        </div>
+    </div>
+    </div>
+    <!-- Go to Top Link -->
+    <a href="#home" class="btn btn-primary back-to-top">
+    <i class=" mdi-hardware-keyboard-arrow-up"></i>
+    </a>
+</section>
+
+<section id="copyright">
+    <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+        <p class="copyright-text">
+            © ManyLingo 2016 All right reserved.
+        </p>
+        </div>
+    </div>
+    </div>
+</section>
+</div>
+
+
+<script src="js/jquery-2.1.4.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/ripples.min.js"></script>
+<script src="js/material.min.js"></script>
+<script src="js/wow.js"></script>
+<script src="js/jquery.mmenu.min.all.js"></script>
+<script src="js/count-to.js"></script>
+<script src="js/jquery.inview.min.js"></script>
+<script src="js/classie.js"></script>
+<script src="js/jquery.nav.js"></script>
+<script src="js/smooth-on-scroll.js"></script>
+<script src="js/smooth-scroll.js"></script>
+<script src="js/main.js"></script>
+
+
+
+<script>
+    $(document).ready(function() {
+        // This command is used to initialize some elements and make them work properly
+        $.material.init();
+    });
+</script>
